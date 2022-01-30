0cfcb9ab9975b037ea005d1c4758cdd8bb315249
SentixDev <sentixdev@protonmail.com>
2022-01-30 03:16:23 +0100

    try catch for invalid images on startup

--- src/main/kotlin/dev/sentix/squaremarker/marker/API.kt
+++ src/main/kotlin/dev/sentix/squaremarker/marker/API.kt
@@ -1,7 +1,10 @@
 package dev.sentix.squaremarker.marker
 
+import dev.sentix.squaremarker.Components
 import dev.sentix.squaremarker.Config
+import dev.sentix.squaremarker.Lang
 import dev.sentix.squaremarker.SquareMarker
+import org.bukkit.Bukkit
 import xyz.jpenilla.squaremap.api.BukkitAdapter
 import xyz.jpenilla.squaremap.api.Key
 import xyz.jpenilla.squaremap.api.SimpleLayerProvider
@@ -13,7 +16,7 @@ object API {
 
     val markerIconKey: Key = Key.of("squaremarker_marker_icon_")
 
-    val providerMap: MutableMap<String, MarkerTask> = HashMap()
+    private val providerMap: MutableMap<String, MarkerTask> = HashMap()
 
     fun init() {
 
@@ -27,18 +30,27 @@ object API {
             mapWorld.layerRegistry()
                 .register(Key.of("squaremarker_" + BukkitAdapter.bukkitWorld(mapWorld).uid + "_marker"), provider)
             val task = MarkerTask(mapWorld, provider)
-            SquareMarker.main.let { task.runTaskTimerAsynchronously(it, 0, 20L * 3) }
+            SquareMarker.main.let { task.runTaskTimerAsynchronously(it, 0, 20L * 2) }
             providerMap[BukkitAdapter.bukkitWorld(mapWorld).uid.toString()] = task
         }
+
     }
 
     private fun registerIcons() {
         SquaremapProvider.get().iconRegistry()
             .register(markerIconKey, ImageIO.read(URL(Config.ICON_URL)))
         for (marker in MarkerService.getMarkerList()) {
-            if (marker.iconUrl != "") {
-                SquaremapProvider.get().iconRegistry()
-                    .register(Key.of("squaremarker_marker_icon_${marker.id}"), ImageIO.read(URL(marker.iconUrl)))
+            println(marker.iconUrl)
+            if (marker.iconUrl.isNotBlank()) {
+                try {
+                    SquaremapProvider.get().iconRegistry()
+                        .register(Key.of("squaremarker_marker_icon_${marker.id}"), ImageIO.read(URL(marker.iconUrl)))
+                } catch (ex: Exception) {
+                    ex.printStackTrace()
+                    Components.send(Bukkit.getConsoleSender(), "")
+                    Components.send(Bukkit.getConsoleSender(), "${Lang.PREFIX} There is an invalid url in your marker.json. Please fix \"${marker.iconUrl}\"!")
+                    Components.send(Bukkit.getConsoleSender(), "")
+                }
             }
         }
     }
@@ -47,6 +59,5 @@ object API {
         providerMap.values.forEach(MarkerTask::disable)
         providerMap.clear()
     }
-
 
 }
