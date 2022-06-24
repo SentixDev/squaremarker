1ce3d4e2ee797d5b66149f03523384fbdac85565
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2022-06-24 14:53:13 -0700

    Use cloud-minecraft-extras for help menu generation

--- common/src/main/kotlin/command/commands/HelpCommand.kt
+++ common/src/main/kotlin/command/commands/HelpCommand.kt
@@ -1,12 +1,18 @@
 package dev.sentix.squaremarker.command.commands
 
+import cloud.commandframework.CommandHelpHandler
+import cloud.commandframework.arguments.standard.StringArgument
 import cloud.commandframework.context.CommandContext
+import cloud.commandframework.minecraft.extras.AudienceProvider
 import cloud.commandframework.minecraft.extras.MinecraftExtrasMetaKeys
+import cloud.commandframework.minecraft.extras.MinecraftHelp
 import dev.sentix.squaremarker.Components
 import dev.sentix.squaremarker.SquareMarker
 import dev.sentix.squaremarker.command.Commander
 import dev.sentix.squaremarker.command.Commands
 import dev.sentix.squaremarker.command.SquaremarkerCommand
+import net.kyori.adventure.text.format.NamedTextColor
+import net.kyori.adventure.text.format.TextColor
 
 class HelpCommand(plugin: SquareMarker, commands: Commands) :
     SquaremarkerCommand(
@@ -14,92 +20,46 @@ class HelpCommand(plugin: SquareMarker, commands: Commands) :
         commands
     ) {
 
+    private val help = createHelp()
+
     override fun register() {
-        this.commands.registerSubcommand { builder ->
+        val helpQueryArgument = StringArgument.newBuilder<Commander>("query")
+            .greedy()
+            .asOptional()
+            .withSuggestionsProvider { context: CommandContext<Commander>, _: String ->
+                val indexHelpTopic = commands.commandManager.createCommandHelpHandler()
+                    .queryHelp(context.sender, "") as CommandHelpHandler.IndexHelpTopic<Commander>
+                indexHelpTopic.entries.map { it.syntaxString }.toList()
+            }
+            .build()
+
+        commands.registerSubcommand { builder ->
             builder.literal("help")
+                .argument(helpQueryArgument)
                 .meta(MinecraftExtrasMetaKeys.DESCRIPTION, Components.parse("Show marker help."))
                 .permission("squaremarker.help")
-                .handler(this::execute)
+                .handler(::execute)
         }
     }
 
     private fun execute(context: CommandContext<Commander>) {
-        val sender = context.sender
-
-        sendHelpMessage(sender)
-
+        help.queryCommands(context.getOrDefault("query", "")!!, context.sender)
     }
 
-    private fun sendHelpMessage(sender: Commander) {
-        val gradient = Components.gradient("<b>Marker</b>")
-        val div =
-            "<dark_gray>» <dark_gray><st>-------------<reset> <gray>× $gradient <gray>× <dark_gray><st>-------------<reset> <dark_gray>«"
-
-        Components.send(sender, "")
-        Components.send(sender, div)
-        Components.send(sender, "")
-
-        Components.send(sender, " <gray>× <color:#8411FB>Usage:")
-        Components.send(sender, "")
-        Components.send(
-            sender,
-            " <gray>× ${
-                Components.clickable(
-                    "<color:#8411FB>/squaremarker list",
-                    "<color:#8411FB>/squaremarker list",
-                    "suggest_command",
-                    "/squaremarker list"
-                )
-            }"
+    private fun createHelp(): MinecraftHelp<Commander> {
+        val help = MinecraftHelp(
+            "/${squareMarker.config.commandLabel}",
+            AudienceProvider.nativeAudience(),
+            commands.commandManager
         )
-        Components.send(
-            sender,
-            " <gray>× ${
-                Components.clickable(
-                    "<color:#8411FB>/squaremarker set <white>[content]</white> <white>[icon-url]</white>",
-                    "<color:#8411FB>/squaremarker set",
-                    "suggest_command",
-                    "/squaremarker set "
-                )
-            }"
+        help.helpColors = MinecraftHelp.HelpColors.of(
+            TextColor.color(0x5B00FF),
+            NamedTextColor.WHITE,
+            TextColor.color(0xC028FF),
+            NamedTextColor.GRAY,
+            NamedTextColor.DARK_GRAY
         )
-        Components.send(
-            sender,
-            " <gray>× ${
-                Components.clickable(
-                    "<color:#8411FB>/squaremarker update <white><id></white> <white>[content]</white> <white>[icon-url]</white>",
-                    "<color:#8411FB>/squaremarker update",
-                    "suggest_command",
-                    "/squaremarker update "
-                )
-            }"
-        )
-        Components.send(
-            sender,
-            " <gray>× ${
-                Components.clickable(
-                    "<color:#8411FB>/squaremarker remove <white><id></white>",
-                    "<color:#8411FB>/squaremarker remove",
-                    "suggest_command",
-                    "/squaremarker remove "
-                )
-            }"
-        )
-        Components.send(
-            sender,
-            " <gray>× ${
-                Components.clickable(
-                    "<color:#8411FB>/squaremarker show <white><id></white>",
-                    "<color:#8411FB>/squaremarker show",
-                    "suggest_command",
-                    "/squaremarker show "
-                )
-            }"
-        )
-
-        Components.send(sender, "")
-        Components.send(sender, div)
-        Components.send(sender, "")
-
+        help.setMessage(MinecraftHelp.MESSAGE_HELP_TITLE, "squaremarker command help")
+        return help
     }
 }
